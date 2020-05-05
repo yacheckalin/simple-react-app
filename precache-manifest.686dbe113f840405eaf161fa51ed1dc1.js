1ead193e792eb552ea70afb4114d66797fe172fe
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2020-05-05 16:32:02 +0300

    Updates

---
+++ precache-manifest.686dbe113f840405eaf161fa51ed1dc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5382ed7f6e140e9a2487417097f7e5d5",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "78e5b491ebaac707f297",
+    "url": "/simple-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82667cd32964e4a2d613",
+    "url": "/simple-react-app/static/js/2.d669528c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-app/static/js/2.d669528c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e5b491ebaac707f297",
+    "url": "/simple-react-app/static/js/main.95c50f52.chunk.js"
+  },
+  {
+    "revision": "cd2fce8caecce55d06d5",
+    "url": "/simple-react-app/static/js/runtime-main.2f9ff02e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
