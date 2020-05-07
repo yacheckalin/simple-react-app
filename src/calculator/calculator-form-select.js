95436a045ad1b65261b1a35f471c98ac7d091e2b
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2020-05-07 13:51:24 +0300

    custom selects

--- src/calculator/calculator-form-select.js
+++ src/calculator/calculator-form-select.js
@@ -2,17 +2,22 @@ import React from "react";
 
 import styled from "styled-components";
 
-const Style = styled.select`
-  max-width: 98px;
-  width: 100%;
-  border-radius: 10px;
-  border: 2px solid #1a237e;
-  padding: 10px;
-  color: back;
-  font-size: 18px;
-  margin-right: 30px;
-  :focus {
-    outline: none;
+const Style = styled.div`
+  position: relative;
+  display: inline-flex;
+
+  select {
+    max-width: 98px;
+    width: 100%;
+    border-radius: 10px;
+    border: 2px solid #1a237e;
+    padding: 10px;
+    color: back;
+    font-size: 18px;
+    margin-right: 30px;
+    :focus {
+      outline: none;
+    }
   }
 `;
 
@@ -22,11 +27,13 @@ const Select = React.memo(({ defaultValue, optionsList, onChangeHandler }) => {
       defaultValue={defaultValue}
       onChange={(e) => onChangeHandler(e.target.value)}
     >
-      {optionsList.map(([item, prop], index) => (
-        <option key={index} value={item} disabled={prop.disabled}>
-          {item}
-        </option>
-      ))}
+      <select>
+        {optionsList.map(([item, prop], index) => (
+          <option key={index} value={item} disabled={prop.disabled}>
+            {item}
+          </option>
+        ))}
+      </select>
     </Style>
   );
 });
