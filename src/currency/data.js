26c81911e15ed904e614c2e586e7cfa5bb7d1451
Yaroslav Chekalin <yaroslav.checkalin@gmail.com>
2020-05-08 01:02:13 +0300

    changed date format

--- src/currency/data.js
+++ src/currency/data.js
@@ -3,60 +3,36 @@ export const currencyData = [
     favorite: false,
     asset: "EUR/USD",
     quote: "1.12",
-    date: new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-    }).format(Date.now()),
+    date: new Date("2019-04-08"),
   },
   {
     favorite: false,
     asset: "USD/JPY",
     quote: "107.95",
-    date: new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-    }).format(Date.now()),
+    date: new Date("2019-04-11"),
   },
   {
     favorite: false,
     asset: "GBP/USD",
     quote: "1.238",
-    date: new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-    }).format(Date.now()),
+    date: new Date("2019-04-18"),
   },
   {
     favorite: false,
     asset: "AUD/USD",
     quote: "0.7048",
-    date: new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-    }).format(Date.now()),
+    date: new Date("2020-04-28"),
   },
   {
     favorite: false,
     asset: "USD/CHF",
     quote: "0.9888",
-    date: new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-    }).format(Date.now()),
+    date: new Date("2020-04-08"),
   },
   {
     favorite: false,
     asset: "USD/CAD",
     quote: "1.31",
-    date: new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
-    }).format(Date.now()),
+    date: new Date("2019-02-08"),
   },
 ];
--- src/history/history-context-provider.js
+++ src/history/history-context-provider.js
@@ -1,7 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
 
-import _ from "lodash";
-
 import { data } from "./data";
 import { INITIAL_PAGINATION_PAGE, DEFAULT_PAGINATION_LIMIT } from "./constants";
 import { customSort } from "./helpers";
